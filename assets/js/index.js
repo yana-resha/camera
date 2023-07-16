cca091784e56f5fca0b7da2efd94a77d1584dd47
Yana <yana@MacBook-Pro-Yana.local>
2023-07-16 21:45:06 +0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,80 @@
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    let btn = document.querySelector('.btn-photo');
+    let textBlock = document.querySelector('.text-container');
+    let uploudInput = document.querySelector('.upload-photo-input');
+    let modal = document.querySelector('.modal');
+    let myModal = new bootstrap.Modal(modal, {
+      keyboard: false
+    });
+    let modalContent = modal.querySelector('.modal-body');
+
+  function successPosition(pos) {
+      let crd = pos.coords;
+      return `<div>Ваше место положение: <br> Широта: ${crd.latitude}<br> Долгота: ${crd.longitude} <br> Плюс-минус ${crd.accuracy} метров</div>`;
+  }
+  
+  var getPosition = function (options) {
+      return new Promise(function (resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject, options);
+      });
+    }
+  
+  function successUpload(position, file) {
+    let text = successPosition(position);
+    textBlock.innerHTML = text;
+    textBlock.innerHTML += `<div>
+    Имя файла: ${file.name} <br>
+    Размер файла: ${file.size} байт
+    </div>`;
+  }
+
+  function showErrorModal() {
+    modalContent.innerHTML = '';
+    modalContent.innerHTML = 'Для работы с приложением необходимо разрешить доступ к камере и геопозиции!';
+    myModal.show();
+  }
+  
+  function openInput() {
+    uploudInput.click();
+  }
+  
+
+    btn.addEventListener('click', (event) => {
+        textBlock.innerHTML = '';
+        
+        
+          navigator.mediaDevices
+          .getUserMedia({video:true})
+          .then((stream) => {
+  
+            getPosition()
+                .then((position) => {
+                  openInput();
+                }).catch(() => {
+                  showErrorModal();
+                })
+  
+            
+          }).catch(() => {
+            showErrorModal();
+          });
+
+          
+    });
+
+    uploudInput.addEventListener('change', (event) => {
+        textBlock.innerHTML = '';
+        let file = event.target.files[0];
+        if (file) {
+          getPosition()
+          .then((position) => {
+            successUpload(position, file);
+          }).catch((error) => {
+            console.log(error);
+          })
+      }
+        
+    });
+})
